53c4393f21b7ca1c2b87a307ac4a91eb04a30bd2
lunievas <luisa.nievas94@gmail.com>
2020-07-28 13:06:57 -0300

    criando a função fetchUsers e pegando em um array somente os dados que vamos precisar {name, picture, login,nat}

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,1 +1,34 @@
-console.log('uhuhu')
+let globalUsers = [];
+let globalCountries = [];
+let globalUsersCountries = [];
+
+
+async function start(){
+    
+   await fetchUsers ();
+   await fetchCountries ();
+
+   console.log(globalUsers)
+   
+    // hideSpinner ();
+    // mergeUsersAndCountries ();
+    // render ();
+}
+
+async function fetchUsers(){
+    const res = await fetch ('http://localhost:3002/users');
+    const json = await res.json();
+    globalUsers = json.map(({name, picture, login,nat}) =>{
+        return {
+            useriD: login.uuid,
+            userCountry: nat,
+            userName: name.first,
+            userPicture: picture.large,
+        };
+    });
+
+
+}
+
+
+start();
